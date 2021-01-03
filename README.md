ab03fe8d93e1df2e76c2e52df8750ef05eac85de
josefondrej <josef.ondrej@outlook.com>
2021-01-03 22:12:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# REST APIs with Flask & Python
+
+Collection of exercises from [this Udemy course](https://www.udemy.com/course/rest-api-flask-and-python/). 
---
+++ lesson_59.py
@@ -0,0 +1,50 @@
+from flask import Flask
+
+print(__name__)
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Wonderful Store",
+        "items": [
+            {
+                "name": "My Item",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+
+# POST /store data: {name}
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route("/store/<string:name>")  # http://127.0.0.1:5000/store/some_name (name variable will contain some_name
+def get_store(name):
+    pass
+
+
+# GET /store
+@app.route("/store")
+def get_stores():
+    pass
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store(name):
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/get_store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
+
+app.run(port=5000)
